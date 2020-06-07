7a08ae6d884100e2372031641365d61f067b18a8
Luke Slater <tinmachin3@gmail.com>
2020-06-07 14:54:39 +0100

    add owl version and script with robot command to convert obo file to owl version

---
+++ convert_obo_to_owl.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# You will have to install this: http://robot.obolibrary.org/ and add its bin directory to PATH (explained in its documentation)
+robot convert -i mpath.obo -o mpath.owl
